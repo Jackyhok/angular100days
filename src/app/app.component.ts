907ace432004f9f46601afb92e3ca10407e913ff
Bang Nguyen Tran Cong <ntcbang@tma.com.vn>
2022-05-26 15:06:58 +0700

    add extension

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,34 +12,34 @@ export class AppComponent {
     name: 'test',
     pass: '',
     age: 11
-  }
+  };
 
   days = 0;
 
   authors = [
     {
       id: 1,
-      firstName: "Flora",
-      lastName: "Twell",
-      email: "ftwell0@phoca.cz",
-      gender: "Female",
-      ipAddress: "99.180.237.33",
+      firstName: 'Flora',
+      lastName: 'Twell',
+      email: 'ftwell0@phoca.cz',
+      gender: 'Female',
+      ipAddress: '99.180.237.33',
     },
     {
       id: 2,
-      firstName: "Priscella",
-      lastName: "Signe",
-      email: "psigne1@berkeley.edu",
-      gender: "Female",
-      ipAddress: "183.243.228.65",
+      firstName: 'Priscella',
+      lastName: 'Signe',
+      email: 'psigne1@berkeley.edu',
+      gender: 'Female',
+      ipAddress: '183.243.228.65',
     },
     // more data
   ];
 
 
 
-  showInfo(){
-    alert("Day " + this.days);
+  showInfo(): void{
+    alert('Day ' + this.days);
     this.days ++;
   }
 }
