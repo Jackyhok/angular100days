40b25405f73be4f7749316d49705632011d02ffb
Bang Nguyen Tran Cong <ntcbang@tma.com.vn>
2022-05-26 15:04:28 +0700

    day 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular100days';
+  title = 'Angular In 100 Days';
+
+  user = {
+    name: 'test',
+    pass: '',
+    age: 11
+  }
+
+  days = 0;
+
+  authors = [
+    {
+      id: 1,
+      firstName: "Flora",
+      lastName: "Twell",
+      email: "ftwell0@phoca.cz",
+      gender: "Female",
+      ipAddress: "99.180.237.33",
+    },
+    {
+      id: 2,
+      firstName: "Priscella",
+      lastName: "Signe",
+      email: "psigne1@berkeley.edu",
+      gender: "Female",
+      ipAddress: "183.243.228.65",
+    },
+    // more data
+  ];
+
+
+
+  showInfo(){
+    alert("Day " + this.days);
+    this.days ++;
+  }
 }
